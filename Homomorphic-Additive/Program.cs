645d0c0af1663c729143126fdc1e1fbaddc38cff
Zakwan Jaroucheh <40012873@napier.ac.uk>
2019-06-06 11:21:09 +0100

    Add project files.

---
+++ Homomorphic-Additive/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Research.SEAL;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Homomorphic_Additive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int votersCount = 5;
+            ulong[] votes = createSampleVotes(votersCount);
+
+            EncryptionParameters parms = new EncryptionParameters(SchemeType.BFV);
+            parms.PolyModulusDegree = 2048;
+            parms.CoeffModulus = DefaultParams.CoeffModulus128(polyModulusDegree: 2048);
+            parms.PlainModulus = new SmallModulus(1 << 8);
+
+            SEALContext context = SEALContext.Create(parms);
+
+            IntegerEncoder encoder = new IntegerEncoder(context);
+
+            KeyGenerator keygen = new KeyGenerator(context);
+            PublicKey publicKey = keygen.PublicKey;
+            SecretKey secretKey = keygen.SecretKey;
+
+            Encryptor encryptor = new Encryptor(context, publicKey);
+
+            Evaluator evaluator = new Evaluator(context);
+            Decryptor decryptor = new Decryptor(context, secretKey);
+
+            int value1 = 5;
+            Plaintext plain1 = encoder.Encode(value1);
+            Console.WriteLine($"Encoded {value1} as polynomial {plain1.ToString()} (plain1)");
+
+            int value2 = -7;
+            Plaintext plain2 = encoder.Encode(value2);
+            Console.WriteLine($"Encoded {value2} as polynomial {plain2.ToString()} (plain2)");
+
+            Ciphertext encrypted1 = new Ciphertext();
+            Ciphertext encrypted2 = new Ciphertext();
+            Console.Write("Encrypting plain1: ");
+            encryptor.Encrypt(plain1, encrypted1);
+            Console.WriteLine("Done (encrypted1)");
+
+            Console.Write("Encrypting plain2: ");
+            encryptor.Encrypt(plain2, encrypted2);
+            Console.WriteLine("Done (encrypted2)");
+
+            Console.WriteLine($"Noise budget in encrypted1: {decryptor.InvariantNoiseBudget(encrypted1)} bits");
+            Console.WriteLine($"Noise budget in encrypted2: {decryptor.InvariantNoiseBudget(encrypted2)} bits");
+
+            evaluator.NegateInplace(encrypted1);
+            Console.WriteLine($"Noise budget in -encrypted1: {decryptor.InvariantNoiseBudget(encrypted1)} bits");
+
+            evaluator.AddInplace(encrypted1, encrypted2);
+
+            Console.WriteLine($"Noise budget in -encrypted1 + encrypted2: {decryptor.InvariantNoiseBudget(encrypted1)} bits");
+            evaluator.MultiplyInplace(encrypted1, encrypted2);
+            Console.WriteLine($"Noise budget in (-encrypted1 + encrypted2) * encrypted2: {decryptor.InvariantNoiseBudget(encrypted1)} bits");
+            Plaintext plainResult = new Plaintext();
+            Console.Write("Decrypting result: ");
+            decryptor.Decrypt(encrypted1, plainResult);
+            Console.WriteLine("Done");
+
+            Console.WriteLine($"Plaintext polynomial: {plainResult.ToString()}");
+
+            Console.WriteLine($"Decoded integer: {encoder.DecodeInt32(plainResult)}");
+        }
+
+        static ulong[] createSampleVotes(int size)
+        {
+            Random random = new Random();
+            ulong[] votes = new ulong[size];
+            for (int i = 0; i < size; i++)
+            {
+                votes[i] = (ulong)random.Next(1, 3);
+            }
+            return votes;
+        }
+
+    }
+}
---
+++ Homomorphic-Encyption/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Homomorphic_Encyption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int size = 512;
+            UInt64 p = getP(size);
+            UInt64 g = getG(size);
+
+            int votersCount = 5;
+            ulong[] v = createSampleVotes(votersCount);
+            ulong[] x = createSamplePrivateValues(votersCount);
+            // Voting keys (broadcast by each voter)
+            ulong[] y = createVotingKeys(g, p, x);
+            // Voter values
+            ulong[] V = createVotingKeys(g, p, v);
+            // Each voter calculates Y[i]
+            ulong[] Y = calculateY(g, p, y);
+            ulong[] registeredVotes = calculateVotes(Y, x, V, p);
+            ulong result = calculateResult(registeredVotes, p);
+
+            Console.WriteLine("p={0}, g={1}", p, g);
+            Console.WriteLine("v=[{0}]", string.Join(", ", v));
+            Console.WriteLine("x=[{0}]", string.Join(", ", x));
+            Console.WriteLine("y=[{0}]", string.Join(", ", y));
+            Console.WriteLine("V=[{0}]", string.Join(", ", V));
+            Console.WriteLine("Y=[{0}]", string.Join(", ", Y));
+            Console.WriteLine("registeredVotes=[{0}]", string.Join(", ", registeredVotes));
+            bruteforce(result, g, p);
+            Console.ReadLine();
+        }
+
+        static void bruteforce(ulong result, ulong g, ulong p)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                ulong gm = ((ulong) Math.Pow(g, i)) % p;
+
+                if (result.Equals(gm))
+                {
+                    Console.WriteLine("Total votes: {0}", i);
+                    break;
+                }
+            }
+        }
+
+        static ulong calculateResult(ulong[] registeredVotes, ulong p)
+        {
+            ulong result = 1;
+
+            for (int i = 0; i < registeredVotes.Length; i++)
+            {
+                result *= registeredVotes[i];
+            }
+            return result % p;
+        }
+
+        static ulong[] calculateVotes(ulong[] Y, ulong[] x, ulong[] V, ulong p)
+        {
+            ulong[] registeredVotes = new ulong[Y.Length];
+            for (int i = 0; i < Y.Length; i++)
+            {
+                registeredVotes[i] = ((ulong)Math.Pow(Y[i], x[i])) % p * V[i];
+            }
+            return registeredVotes;
+        }
+
+        static ulong[] calculateY(ulong g, ulong p, ulong[] y)
+        {
+            ulong[] Y = new ulong[y.Length];
+
+            for (int i = 0; i < y.Length; i++)
+            {
+                ulong res = 1;
+
+                for (int j = 0; j <= i-1; j++)
+                {
+                    res *= y[j];
+                    res = res % p;
+                }
+
+                for (int j = i+1; j < y.Length; j++)
+                {
+                    res *= (ulong) modInverse((int) y[j], (int) p);
+                    res = res % p;
+                }
+
+                Y[i] = res % p;
+            }
+
+            return Y;
+        }
+
+        static int modInverse(int num, int p)
+        {
+            num = num % p;
+            for (int i=1; i<p; i++)
+            {
+                if (num * i % p == 1) return i;
+            }
+            return 1;
+        }
+
+        static ulong[] createVotingKeys(ulong g, ulong p, ulong[] x)
+        {
+            ulong[] y = new ulong[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = makeG(g, x[i], p);
+            }
+            return y;
+        }
+
+        static ulong[] createSampleVotes(int size)
+        {
+            Random random = new Random();
+            ulong[] votes = new ulong[size];
+            for (int i = 0; i < size; i++) {
+                votes[i] = (ulong) random.Next(1, 3);
+            }
+            return votes;
+        }
+
+        static ulong[] createSamplePrivateValues(int size)
+        {
+            Random random = new Random();
+            ulong[] votes = new ulong[size];
+            for (int i = 0; i < size; i++)
+            {
+                votes[i] = (ulong) random.Next(1, 10);
+            }
+            return votes;
+        }
+
+        static ulong makeG(ulong g, ulong v, ulong p)
+        {
+            return ((ulong) Math.Pow(g, v)) % p;
+        }
+
+        static UInt64 hexToInt(string hexValue)
+        {
+            return Convert.ToUInt64(hexValue, 16);
+        }
+
+        // TODO
+        static UInt64 getG(int size)
+        {
+            if (size.Equals(512))
+            {
+                return 2;// hexToInt("11");
+            }
+            else if (size.Equals(768)) {
+                return hexToInt("30");
+            }
+            else if (size.Equals(1024)) {
+                return hexToInt("F7");
+            }
+            return 0;
+        }
+
+        static UInt64 getP(int size)
+        {
+            if (size.Equals(512)) {
+                return 11;// hexToInt("11");
+            } else if (size.Equals(768)) {
+                return hexToInt("E9");
+            } else if (size.Equals(1024)) {
+                return hexToInt("FD");
+            }
+            return 0;
+        }
+    }
+}
